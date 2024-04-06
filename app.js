e6883226762611702d70bece63421d1a859d4380
ALPER <icmyagmuracikgoz@gmail.com>
2024-04-06 23:10:21 +0300

    added key

--- app.js
+++ app.js
@@ -4,10 +4,58 @@ for (let i =0  ; i<numberOfDrumButtons; i++){
 
 document.querySelectorAll(".drum")[i].addEventListener("click", function () {
 
-    let audio = new Audio("sounds/tom-1.mp3");
-    audio.play();
+    let buttonInnerHtml= this.innerHTML;
+    makeSound(buttonInnerHtml);
+
  });
 
 }
 
+document.addEventListener("keypress", function(event){
+   makeSound(event.key);
+});
+
+function makeSound(key){
+
+    switch(key){
+
+        case "w":
+            let tom1=new Audio("./sounds/tom-1.mp3");
+            tom1.play();
+        break;
+    
+        case "a":
+            let tom2=new Audio("./sounds/tom-2.mp3");
+            tom2.play();
+        break;
+    
+        case "s":
+            let tom3=new Audio("./sounds/tom-3.mp3");
+            tom3.play();
+        break;
+    
+        case "d":
+            let tom4=new Audio("./sounds/tom-4.mp3");
+            tom4.play();
+        break;
+    
+        case "j":
+            let crash=new Audio("./sounds/crash.mp3");
+            crash.play();
+        break;
+    
+        case "k":
+            let kick=new Audio("./sounds/kick-bass.mp3");
+            kick.play();
+        break;
+    
+        case "l":
+            let snare=new Audio("./sounds/snare.mp3");
+            snare.play();
+        break;
+    
+        default:console.log();
+       }
+
+}
 
